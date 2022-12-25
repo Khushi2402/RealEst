322c02ec2125ff01faa0be004e8d38c0046aa818
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2022-12-25 22:38:42 +0530

    Added Shimmer Effects

--- app/src/main/java/com/example/project/Home.kt
+++ app/src/main/java/com/example/project/Home.kt
@@ -2,10 +2,37 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        navController = findNavController(R.id.NavHostFragment)
+        bottomNavigationView  = findViewById(R.id.bottomNavigationView)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.favouritesFragment,
+                R.id.profileFragment
+                )
+            )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/project/Property.kt
@@ -0,0 +1,4 @@
+package com.example.project
+
+class Property (var image:Int, var name:String, var type:String, var location:String, var size:Int, var detail:String) {
+}
